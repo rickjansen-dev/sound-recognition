e11bb3f62e457cd7a34bed862b3d128cc969c7d2
auspex-nl <r.b.j.jansen@gmail.com>
2013-04-21 15:38:33 +0200

    updated matlab haar

--- matlab/haar.m
+++ matlab/haar.m
@@ -5,7 +5,7 @@ function [ output_args ] = haar( filename )
 Wfilters = [2,4,6,8,10,12,14,16,18,20];
 alpha = 1;
 frames = 40;
-framesize = samples/frames;
+framesize = 8000/frames;
 Wshift = 0;
 
 fprintf('Selected frame size: %d', framesize);
@@ -17,32 +17,38 @@ fprintf('Selected frame size: %d', framesize);
 % the current frame to save memory (1-frame's worth of 32bit ints instead
 % of a complete 1sec sample, which would consume 32KB - the Arduino Due 
 % has 96 in total...)
-IS = zeros(size(samples,1));
+global IS;
 Icurrent = 0;
+IS = zeros(size(samples,2));
 
-for i=1:size(samples,1)
-   Icurrent = Icurrent + samples(i);
+%samples(1,2)
+
+for i=1:size(samples,2)
+    Icurrent = Icurrent + samples(1,i);
    IS(i) = Icurrent;
 end
 
 xm = zeros(frames);
 t=0;
 
-for j=1:frames
-   xmcurrent = zeros(length(Wfilters));
+%for j=1:frames
+for j=1:2    
+   xmcurrent = zeros(size(Wfilters));
    for wf=1:length(Wfilters)
        Wshift = alpha * Wfilters(wf);
        xmcurrent_filter = 0;
        for n=1:((framesize-Wfilters(wf))/Wshift + 1)
            t = j*framesize + n;
+           %fprintf(' OneFilterValue: %d - %d - %d', IS(t+Wfilters(wf)),2 * IS(t+Wfilters(wf)/2),IS(t));
            onefiltervalue = IS(t+Wfilters(wf)) - 2 * IS(t+Wfilters(wf)/2) + IS(t);
            xmcurrent_filter = xmcurrent_filter + onefiltervalue;
        end
-       fprintf('filter wf = %d, value: %d\n',wf,xmcurrent_filter);
+       %fprintf('filter wf = %d, value: %d\n',Wfilters(wf),xmcurrent_filter);
+       xmcurrent(wf) = xmcurrent_filter;
    end
    fprintf('frame %d\n', j);
    xmcurrent
-   xm(j) = xmcurrent;
+   %xm(j) = xmcurrent;
 end
 % for each frame, do:
     % -calc IS values=C only-
