f62164bfc2c2bc1de8ce62082b7881f23409aaaf
auspex-nl <r.b.j.jansen@gmail.com>
2013-04-22 23:57:53 +0200

    added haar C

--- common/src/recog/haar.h
+++ common/src/recog/haar.h
@@ -4,5 +4,15 @@
 #include "recog.h"
 
 void haar_process();
+void haar_calc_IS(int);
+
+//define filters
+extern int16_t Wfilters[];
+extern int16_t number_of_filters;
+extern int16_t alpha;
+extern int16_t Wshift;
+extern int32_t IS_last_frame;
+extern int32_t IS_last_offset;
+extern int32_t IS[FRAME_SIZE];
 
 #endif
--- pc/recog/test.sh
+++ pc/recog/test.sh
@@ -1,5 +1,5 @@
 #avconv -i /media/rick/8156-6047/audio/sounds/44khz\ 16bit\ wav/117614__soundmary__door-close.wav -f u16le -acodec pcm_s8 -ar 8000 -loglevel quiet - | ./bin/recog
 
-echo "Outputting bitsize $1, with Endianess: $2"
+echo "Outputting bitsize 16, with Endianess: little"
 
-sox /media/rick/8156-6047/audio/sounds/44khz\ 16bit\ wav/117614__soundmary__door-close.wav -e signed-integer -b $1 --endian $2 -r 8000 -c 1 -t raw - | ./bin/recog
+sox /home/rick/audio/sounds/44khz\ 16bit\ wav/117614__soundmary__door-close.wav -e signed-integer -b 16 --endian little -r 8000 -c 1 -t raw - | ./bin/recog
